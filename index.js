1f27f85e526e18dff88acb14bee126d7a9c5bdb7
dannyfrelink <danny-frelink@hotmail.com>
2022-04-20 12:10:59 +0200

    create routes

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
-const app = express()
+const app = express();
+const PORT = 4141;
+const htmlPath = `${__dirname}/public/html/`;
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(`${htmlPath}index.html`);
+});
+app.get('/detail', (req, res) => {
+    res.sendFile(`${htmlPath}detail.html`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Application started on port: http://localhost:${PORT}`);
+});
