a45714a7447c6c6575c614dd48216ce281f6dfa7
dannyfrelink <danny-frelink@hotmail.com>
2022-04-22 09:54:56 +0200

    trying multiple articles

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,40 @@
-const anchor = document.querySelector('a');
-const article = document.querySelector('article');
-article.style.top = "100px";
-article.style.left = "100px";
+const anchors = document.querySelectorAll('a');
+const articles = document.querySelectorAll('article');
+for (let i = 0; i < articles.length; i++) {
+    articles[i].style.top = "100px";
+    articles[i].style.left = "100px";
+}
 let clickCounter = 0;
 
-anchor.addEventListener('click', event => {
-    event.preventDefault();
+for (let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener('click', event => {
+        event.preventDefault();
+        console.log(articles[i])
 
-    clickCounter = clickCounter + 1;
-    if(clickCounter > 1) {
-        document.removeEventListener('keydown', moveKeys);
-        clickCounter = 0;
-    } else {
-        document.addEventListener('keydown', moveKeys);
-    }
-});
+        clickCounter = clickCounter + 1;
+        if(clickCounter > 1) {
+            articles[i].removeEventListener('keydown', moveKeys);
+            clickCounter = 0;
+            articles.forEach(article => {
+                article.classList.remove('dragged');
+            });
+        } else {
+            articles[i].classList.remove('dragged');
+            articles[i].classList.add('dragged');
+            articles[i].addEventListener('keydown', moveKeys);
+        }
+    });
 
-const moveKeys = e => {
-    const keyCode = e.keyCode;
-    if(keyCode === 37) {
-        article.style.left = parseInt(article.style.left) - 10 + "px";
-    } else if(keyCode === 38) {
-        article.style.top = parseInt(article.style.top) - 10 + "px";
-    } else if(keyCode === 39) {
-        article.style.left = parseInt(article.style.left) + 10 + "px";
-    } else if(keyCode === 40) {
-        article.style.top = parseInt(article.style.top) + 10 + "px";
+    const moveKeys = e => {
+        const keyCode = e.keyCode;
+        if(keyCode === 37) {
+            articles[i].style.left = parseInt(articles[i].style.left) - 10 + "px";
+        } else if(keyCode === 38) {
+            articles[i].style.top = parseInt(articles[i].style.top) - 10 + "px";
+        } else if(keyCode === 39) {
+            articles[i].style.left = parseInt(articles[i].style.left) + 10 + "px";
+        } else if(keyCode === 40) {
+            articles[i].style.top = parseInt(articles[i].style.top) + 10 + "px";
+        }
     }
 }
