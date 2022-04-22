2d0890f16eca85f70847864b8b78e02898aa94be
dannyfrelink <danny-frelink@hotmail.com>
2022-04-22 12:38:05 +0200

    style placement articles

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ const anchors = document.querySelectorAll('a');
 const articles = document.querySelectorAll('article');
 for (let i = 0; i < articles.length; i++) {
     articles[i].style.top = `${Math.floor(Math.random() * 400) + 100}px`;
-    articles[i].style.left = `${Math.floor(Math.random() * 500) + 100}px`;
+    articles[i].style.left = `${Math.floor(Math.random() * 500) + 200}px`;
 }
 
 for (let i = 0; i < anchors.length; i++) {
@@ -18,13 +18,13 @@ for (let i = 0; i < anchors.length; i++) {
 
     const moveKeys = e => {
         const keyCode = e.keyCode;
-        if(keyCode === 37 || keyCode === 72) {
+        if (keyCode === 37 || keyCode === 72) {
             articles[i].style.left = parseInt(articles[i].style.left) - 10 + "px";
-        } else if(keyCode === 38 || keyCode === 75) {
+        } else if (keyCode === 38 || keyCode === 75) {
             articles[i].style.top = parseInt(articles[i].style.top) - 10 + "px";
-        } else if(keyCode === 39 || keyCode === 76) {
+        } else if (keyCode === 39 || keyCode === 76) {
             articles[i].style.left = parseInt(articles[i].style.left) + 10 + "px";
-        } else if(keyCode === 40 || keyCode === 74) {
+        } else if (keyCode === 40 || keyCode === 74) {
             articles[i].style.top = parseInt(articles[i].style.top) + 10 + "px";
         }
     }
