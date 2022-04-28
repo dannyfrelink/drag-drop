a7de82bbce4c7dae80a7a8f632a1da27a13202b7
dannyfrelink <danny-frelink@hotmail.com>
2022-04-28 17:40:24 +0200

    add port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4141;
+const PORT = process.env.PORT || 4141;
 const htmlPath = `${__dirname}/public/html/`;
 
 app.use(express.static('public'));
